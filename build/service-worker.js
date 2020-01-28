812e8a6645af6138d5a434cb2ec164bf5c6bde8f
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-01-28 11:36:33 +0200

    autocommit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d003efa1cd16b79902ca5782e1acd71f.js"
+  "/precache-manifest.d4dceb4514e520f925a6fa30bf3eb1a8.js"
 );
 
 self.addEventListener('message', (event) => {
